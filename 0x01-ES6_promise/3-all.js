bf8215758a888e56a018f0dbf89f1c2421d953e8
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-09 17:34:51 +0000

    promise all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const promises = [createUser(), uploadPhoto()];
   return Promise.all(promises).then((values) => {
-    console.log(values[1].body, values[0].firstName, values[0].lastName);
+    console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }
