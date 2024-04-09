5619d350d2b525629e79858cbea2e48d1ee78be2
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-09 17:26:47 +0000

    promis all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  const promises = [createUser(), uploadPhoto()];
+  Promise.all(promises).then((values) => {
+    console.log(values[1].body, values[0].firstName, values[0].lastName);
+  });
+}
