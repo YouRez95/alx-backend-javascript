0078657576484d0a9ab3762f89958773cc8ba3db
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-10 22:06:11 +0000

    fix the any promise

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
-export default function loadBalancer(chinaDownload, USDownload) {
+export default async function loadBalancer(chinaDownload, USDownload) {
   const promises = [chinaDownload, USDownload];
   return Promise.any(promises).then((value) => value);
 }
