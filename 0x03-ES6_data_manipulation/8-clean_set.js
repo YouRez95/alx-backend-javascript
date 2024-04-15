c9e116f5bd2c1d1227a277cf7e23922c68a15ae3
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-15 17:46:50 +0100

    fix clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const text = [];
-  if (!set || !startString) {
-    return ''
+  if (!set || !startString || !(set instanceof Set)) {
+    return '';
   }
   set.forEach((value) => {
     if (value.startsWith(startString)) {
