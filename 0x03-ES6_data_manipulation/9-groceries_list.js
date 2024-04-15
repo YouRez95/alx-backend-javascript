39e369401673e27174e344edffb5fc1f682f75c2
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-15 18:03:50 +0100

    fix map groceries

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,10 @@
 export default function groceriesList() {
-  const values = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 1]];
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 1],
+  ];
   return new Map(values);
 }
