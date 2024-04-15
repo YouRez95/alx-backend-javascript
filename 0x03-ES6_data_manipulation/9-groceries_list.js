2280d6d49d28cd17cc28d4b64b4321b2b1fc3d37
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-15 16:18:25 +0100

    create Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const grocerieMap = new Map();
+  grocerieMap.set('Apples', 10);
+  grocerieMap.set('Tomatoes', 10);
+  grocerieMap.set('Pasta', 1);
+  grocerieMap.set('Rice', 1);
+  grocerieMap.set('Banana', 1);
+  return grocerieMap;
+}
