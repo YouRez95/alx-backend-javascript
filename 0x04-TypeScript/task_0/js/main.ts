a0dd6efc1d87d139654e05437b453dbf0fa9eb8e
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-17 11:03:04 +0100

    create an interface

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,38 @@
+interface Student {
+  firstName: string
+  lastName: string
+  age: number
+  location: string
+};
+
+const student1: Student = {
+  firstName: "Youness",
+  lastName: "Alouani",
+  age: 28,
+  location: "Maroc"
+};
+
+const student2: Student = {
+  firstName: "Mario",
+  lastName: "Freen",
+  age: 29,
+  location: "France"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+
+studentsList.forEach((student) => {
+  const row = document.createElement('tr');
+
+  const cellName = document.createElement('td');
+  cellName.textContent = student.firstName;
+
+  const cellLocation = document.createElement('td');
+  cellLocation.textContent = student.location;
+
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  table.appendChild(row);
+})
