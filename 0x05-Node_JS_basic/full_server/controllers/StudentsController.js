150135316e57823d398722518846d682f9492a11
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-04 21:42:15 +0100

    add comparaison

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,7 +6,18 @@ class StudentsController {
   static getAllStudents(request, response) {
     readDatabase(path).then((data) => {
       const responses = ['This is the list of our students'];
-      for (const [key, value] of Object.entries(data)) {
+
+      const compareFonction = (a, b) => {
+        if (a[0].toLowerCase() < b[0].toLowerCase()) {
+          return -1;
+        }
+        if (a[0].toLowerCase() > b[0].toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      };
+
+      for (const [key, value] of Object.entries(data).sort(compareFonction)) {
         responses.push(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
       }
       return response.status(200).send(responses.join('\n'));
