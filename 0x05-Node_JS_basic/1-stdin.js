e3028e445be07b2e17b234da72f0ff079a1a4aad
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-04 11:59:55 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,10 @@
  * Using Process stdin
  */
 
-const readLine = require('readline');
-
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
+  process.stdin.on('close', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
 });
