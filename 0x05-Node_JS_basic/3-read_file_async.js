906a6666ec77c9c3598efd7ecdcba385ae21df7f
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-04 12:51:49 +0100

    reading file async with node js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+/**
+ * Reading a file asynchronously with Node JS
+ */
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        console.log('Error: Cannot load the database');
+        reject(err);
+      }
+      const fields = {};
+      const students = data.trim().split('\n').slice(1);
+
+      for (const student of students) {
+        const studentInfo = student.split(',');
+        if (fields[studentInfo[studentInfo.length - 1]] === undefined) {
+          fields[studentInfo[studentInfo.length - 1]] = [studentInfo[0]];
+        } else {
+          fields[studentInfo[studentInfo.length - 1]].push(studentInfo[0]);
+        }
+      }
+
+      console.log(`Number of students: ${students.length}`);
+      for (const [key, value] of Object.entries(fields)) {
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      }
+      resolve();
+    });
+  });
+}
+module.exports = countStudents;
