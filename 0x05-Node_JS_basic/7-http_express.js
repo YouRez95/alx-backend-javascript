d2f3bfbc819767b068c7338b73cab51e52673ff9
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-04 21:47:28 +0100

    fix readfile_async

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -7,7 +7,7 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        return reject('Cannot load the database');
+        return reject(new Error('Cannot load the database'));
       }
       const fields = {};
       const students = data.trim().split('\n').slice(1);
