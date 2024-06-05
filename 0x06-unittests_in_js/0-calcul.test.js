21306f5597c5067b3fc77b65719f0c3e18b1c73c
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-05 19:45:09 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,18 +4,18 @@ const calculateNumber = require("./0-calcul.js");
 describe('calculateNumber', function () {
 
   it('should return 4', function () {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('should return 5', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
   it('should return 5', function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
   it('should return 6', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
