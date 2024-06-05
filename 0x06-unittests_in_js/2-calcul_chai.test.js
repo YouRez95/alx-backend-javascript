3389132df7618403f196b895141140785b528134
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-05 20:21:37 +0100

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,59 +5,59 @@ const calculateNumber = require('./1-calcul');
 describe('calculateNumber', function () {
 
   it('should return 12', function () {
-    expect(calculateNumber('SUM', 10.0, 2.0)).to.be.equal(12)
+    expect(calculateNumber('SUM', 10.0, 2.0)).to.equal(12)
   });
 
   it('should return 13', function () {
-    expect(calculateNumber('SUM', 10.7, 2.4)).to.be.equal(13);
+    expect(calculateNumber('SUM', 10.7, 2.4)).to.equal(13);
   });
 
   it('should return 11', function () {
-    expect(calculateNumber('SUM', 7.5, 2.9)).to.be.equal(11);
+    expect(calculateNumber('SUM', 7.5, 2.9)).to.equal(11);
   });
 
   it('should return 9', function () {
-    expect(calculateNumber('SUM', 7.4999, 2.49999)).to.be.equal(9);
+    expect(calculateNumber('SUM', 7.4999, 2.49999)).to.equal(9);
   });
 
   it('should return 8', function () {
-    expect(calculateNumber('SUBTRACT', 10.0, 2.0)).to.be.equal(8);
+    expect(calculateNumber('SUBTRACT', 10.0, 2.0)).to.equal(8);
   });
 
   it('should return 9', function () {
-    expect(calculateNumber('SUBTRACT', 10.7, 2.4)).to.be.equal(9);
+    expect(calculateNumber('SUBTRACT', 10.7, 2.4)).to.equal(9);
   });
 
   it('should return 5', function () {
-    expect(calculateNumber('SUBTRACT', 7.5, 2.9)).to.be.equal(5);
+    expect(calculateNumber('SUBTRACT', 7.5, 2.9)).to.equal(5);
   });
 
   it('should return 5', function () {
-    expect(calculateNumber('SUBTRACT', 7.4999, 2.49999)).to.be.equal(5);
+    expect(calculateNumber('SUBTRACT', 7.4999, 2.49999)).to.equal(5);
   });
 
   it('should return 5', function () {
-    expect(calculateNumber('DIVIDE', 10.0, 2.0)).to.be.equal(5);
+    expect(calculateNumber('DIVIDE', 10.0, 2.0)).to.equal(5);
   });
 
   it('should return 6', function () {
-    expect(calculateNumber('DIVIDE', 11.7, 2.4)).to.be.equal(6);
+    expect(calculateNumber('DIVIDE', 11.7, 2.4)).to.equal(6);
   });
 
   it('should return 3.3333333333333335', function () {
-    expect(calculateNumber('DIVIDE', 9.5, 2.9)).to.be.equal(3.3333333333333335);
+    expect(calculateNumber('DIVIDE', 9.5, 2.9)).to.equal(3.3333333333333335);
   });
 
   it('should return 4', function () {
-    expect(calculateNumber('DIVIDE', 8.4999, 2.49999)).to.be.equal(4);
+    expect(calculateNumber('DIVIDE', 8.4999, 2.49999)).to.equal(4);
   });
 
   it('should return 0', function () {
-    expect(calculateNumber('DIVIDE', 0, 9)).to.be.equal(0);
+    expect(calculateNumber('DIVIDE', 0, 9)).to.equal(0);
   });
 
   it('should return Error', function () {
-    expect(calculateNumber('DIVIDE', 7, 0)).to.be.equal('Error');
+    expect(calculateNumber('DIVIDE', 7, 0)).to.equal('Error');
   });
 
 });
