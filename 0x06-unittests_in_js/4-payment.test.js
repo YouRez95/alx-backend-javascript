5aa5cdc6d51bdd8e68f726cf4e9465bec7fa1b26
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-07 10:49:10 +0100

    stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,19 +4,24 @@ const Utils = require("./utils");
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let calculateNumberSpy;
+  let calculateNumberStub;
+  let consoleSpy;
 
   beforeEach(() => {
-    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
-    calculateNumberSpy.restore();
+    calculateNumberStub.restore();
+    consoleSpy.restore();
   });
 
   it('call Utils.calculateNumber with "SUM", 100, 20', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(calculateNumberSpy.callCount).to.be.equal(1);
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberStub.callCount).to.be.equal(1);
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    expect(consoleSpy.callCount).to.be.equal(1);
   });
 });
