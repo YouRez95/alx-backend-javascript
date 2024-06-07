03fcc21256f7286481a424599c90837770a3e4f6
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-07 11:31:36 +0100

    skip some tests

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,10 +4,9 @@ const getPaymentTokenFromAPI = require("./6-payment_token");
 describe('getPaymentTokenFromAPI', () => {
   it('getPaymentTokenFromAPI(true)', (done) => {
     getPaymentTokenFromAPI(true).then((result) => {
-      console.log(result)
-      expect(result).to.be.an('object')
-      expect(result).to.have.property('data','Successful response from the API')
-      done()
+      expect(result).to.be.an('object');
+      expect(result).to.have.property('data','Successful response from the API');
+      done();
     })
   });
 })
